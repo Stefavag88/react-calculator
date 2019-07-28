6430c7637d4bd2bc02ba33a41d55710d56cd9d93
Vaggelis Stefanakis <stefanakisvag@gmail.com>
2019-07-28 15:06:45 +0300

    Keypad Class to function

--- src/components/KeyPad/index.js
+++ src/components/KeyPad/index.js
@@ -11,33 +11,37 @@ import {
   faEquals
 } from "@fortawesome/free-solid-svg-icons";
 
-class KeyPad extends React.Component {
-  render() {
-    return (
-      <StyledKeyPad>
-        <Operator value={"C"} fontSize={"1.1em"} onClick={this.props.onClearAllClick}/>
-        <Operator value={"CE"} fontSize={"1.1em"} onClick={this.props.onClearScreenClick} />
-        <Operator value={"<-"} icon={faBackspace} onClick={this.props.onBackspaceClick}/>
-        <Operator value={"/"} icon={faDivide} onClick={this.props.onOperatorClick}/>
-        <Number value={7} onClick={this.props.onNumberClick}/>
-        <Number value={8} onClick={this.props.onNumberClick}/>
-        <Number value={9} onClick={this.props.onNumberClick}/>
-        <Operator value={"*"} icon={faTimes} onClick={this.props.onOperatorClick}/>
-        <Number value={4} onClick={this.props.onNumberClick} />
-        <Number value={5} onClick={this.props.onNumberClick} />
-        <Number value={6} onClick={this.props.onNumberClick} />
-        <Operator value={"-"} icon={faMinus} onClick={this.props.onOperatorClick}/>
-        <Number value={1} onClick={this.props.onNumberClick} />
-        <Number value={2} onClick={this.props.onNumberClick} />
-        <Number value={"3"} onClick={this.props.onNumberClick} />
-        <Operator value={"+"} icon={faPlus} onClick={this.props.onOperatorClick}/>
-        <Operator value={"+/-"} fontSize={"1.1em"} onClick={this.props.onReverseSignClick}/>
-        <Number value={0} onClick={this.props.onNumberClick} />
-        <Number value={"."} onClick={this.props.onNumberClick}/>
-        <Operator value={"="} icon={faEquals} onClick={this.props.onEqualsClick}/>
-      </StyledKeyPad>
-    );
-  }
-}
+const KeyPad = ({
+  onNumberClick,
+  onOperatorClick,
+  onClearAllClick,
+  onClearScreenClick,
+  onBackspaceClick,
+  onReverseSignClick,
+  onEqualsClick
+}) => (
+  <StyledKeyPad>
+    <Operator value={"C"} fontSize={"1.1em"} onClick={onClearAllClick} />
+    <Operator value={"CE"} fontSize={"1.1em"} onClick={onClearScreenClick} />
+    <Operator value={"<-"} icon={faBackspace} onClick={onBackspaceClick} />
+    <Operator value={"/"} icon={faDivide} onClick={onOperatorClick} />
+    <Number value={7} onClick={onNumberClick} />
+    <Number value={8} onClick={onNumberClick} />
+    <Number value={9} onClick={onNumberClick} />
+    <Operator value={"*"} icon={faTimes} onClick={onOperatorClick} />
+    <Number value={4} onClick={onNumberClick} />
+    <Number value={5} onClick={onNumberClick} />
+    <Number value={6} onClick={onNumberClick} />
+    <Operator value={"-"} icon={faMinus} onClick={onOperatorClick} />
+    <Number value={1} onClick={onNumberClick} />
+    <Number value={2} onClick={onNumberClick} />
+    <Number value={3} onClick={onNumberClick} />
+    <Operator value={"+"} icon={faPlus} onClick={onOperatorClick} />
+    <Operator value={"+/-"} fontSize={"1.1em"} onClick={onReverseSignClick} />
+    <Number value={0} onClick={onNumberClick} />
+    <Number value={"."} onClick={onNumberClick} />
+    <Operator value={"="} icon={faEquals} onClick={onEqualsClick} />
+  </StyledKeyPad>
+);
 
 export default KeyPad;
